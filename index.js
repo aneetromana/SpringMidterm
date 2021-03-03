667e4bb510193411b16e4ffaa61fcd2ef976cc2a
aneetromana <aneetromana167@gmail.com>
2021-03-03 12:22:10 -0800

    msg

---
+++ index.js
@@ -0,0 +1,9 @@
+function HamburgerClick(){
+    document.querySelector(".menu_items").style.opacity = "100%";
+    document.querySelector(".menu_items").style.display = "flex";
+}
+
+function ExitMenu(){
+  document.querySelector(".menu_items").style.display = "none";
+}
+
