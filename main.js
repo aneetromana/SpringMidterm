4400d8565b6c2c9e48a822c0a9b6c8fd93c71feb
aneetromana <aneetromana167@gmail.com>
2021-03-07 02:32:51 -0800

    commit

--- main.js
+++ main.js
@@ -2,4 +2,4 @@ document.querySelector("#menu").innerHTML+=Menu();
 document.querySelector("#button").innerHTML+=Button();
 document.querySelector("#card").innerHTML+=Card();
 document.querySelector("#main").innerHTML+=Header();
-
+document.querySelector("#title").innerHTML+=Title();
---
+++ titlepage/title.js
@@ -0,0 +1,19 @@
+function Title() {
+    return `
+    <div id="title" style="
+    border-radius: 25px;
+    background: #EEE5FF; 
+    width: 60vw;
+    height: 100vw;  
+    margin:50px;
+    font-family: 'Actor', sans-serif;
+    font-size: 80px;
+    color: #FFFFFF;
+    text-shadow: 1px 1px 0 #DAB8FF,
+                          2px 2px 0 #DAB8FF,
+                         2px 2px 0 #DAB8FF,
+                        2px 2px 0 #DAB8FF;
+                        text-align:center;
+    onclick="HandleOpen()">Kitten Adoption</button>
+    </div> `;
+}
