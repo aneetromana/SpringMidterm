068166bf02ca946712b5d2c1265af21a133cab53
aneetromana <aneetromana167@gmail.com>
2021-03-07 02:22:04 -0800

    commit

---
+++ headercomp/header.js
@@ -0,0 +1,11 @@
+function Header() {
+    return `
+    <div id="header" style="
+    font-size:35px;
+    background: #F2FFF6;
+    width: 60vw;
+    height:6vw;  
+    font-family: 'Actor', sans-serif;
+     onclick="HandleOpen()">Kitten Adoption</button>
+    </div> `;
+}
--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 document.querySelector("#menu").innerHTML+=Menu();
 document.querySelector("#button").innerHTML+=Button();
 document.querySelector("#card").innerHTML+=Card();
+document.querySelector("#main").innerHTML+=Header();
 
